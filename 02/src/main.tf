f9a7cb4caa58f5e3a5ccc59e160d6f2e68f94cbe
Shaw927 <men927ww@gmail.com>
2026-02-25 04:09:26 -0500

    2-3 zadanie

--- 02/src/main.tf
+++ 02/src/main.tf
@@ -1,6 +1,7 @@
 resource "yandex_vpc_network" "develop" {
   name = var.vpc_name
 }
+
 resource "yandex_vpc_subnet" "develop" {
   name           = var.vpc_name
   zone           = var.default_zone
@@ -8,17 +9,23 @@ resource "yandex_vpc_subnet" "develop" {
   v4_cidr_blocks = var.default_cidr
 }
 
+resource "yandex_vpc_subnet" "develop_db" {
+  name = var.vpc_name_db
+  zone = var.vm_db_zone
+  network_id = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.db_cidr
+}
 
 data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu-2004-lts"
+  family = var.vm_web_family
 }
 resource "yandex_compute_instance" "platform" {
-  name        = "netology-develop-platform-web"
-  platform_id = "standard-v1"
+  name        = var.vm_web_name
+  platform_id = var.vm_web_platform_id
   resources {
-    cores         = 2
-    memory        = 2
-    core_fraction = 5
+    cores         = var.vm_web_cores
+    memory        = var.vm_web_memory
+    core_fraction = var.vm_web_core_fraction
   }
   boot_disk {
     initialize_params {
@@ -26,16 +33,44 @@ resource "yandex_compute_instance" "platform" {
     }
   }
   scheduling_policy {
-    preemptible = true
+    preemptible = var.vm_web_preemptible
   }
   network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
-    nat       = true
+    nat       = var.vm_web_nat
   }
 
   metadata = {
-    serial-port-enable = 1
+    serial-port-enable = var.vm_web_serial_port
     ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
   }
 
+}
+
+resource "yandex_compute_instance" "db1" {
+  name        = var.vm_db_name
+  platform_id = var.vm_db_platform_id
+  zone = var.vm_db_zone
+  resources {
+    cores         = var.vm_db_cores
+    memory        = var.vm_db_memory
+    core_fraction = var.vm_db_core_fraction
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = var.vm_db_preemptible
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop_db.id
+    nat       = var.vm_db_nat
+  }
+
+  metadata = {
+    serial-port-enable = var.vm_db_serial_port
+    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+  }
 }
---
+++ 02/src/vms_platform.tf
@@ -0,0 +1,129 @@
+###vm web vars
+
+variable "vm_web_name" {
+  type = string
+  default = "netology-develop-platform-web"
+  description = "Название виртуальной машины"
+}
+
+variable "vm_web_family" {
+  type = string
+  default = "ubuntu-2004-lts"
+  description = "версия ОС"
+}
+
+variable "vm_web_platform_id" {
+  type = string
+  default = "standard-v1"
+  description = "Тип процессора"
+}
+
+variable "vm_web_cores" {
+  type = number
+  default = 2
+  description = "Количество выделенных ядер процеесора"
+}
+
+variable "vm_web_memory" {
+  type = number
+  default = 2
+  description = "Количество выделенной оперативной памяти"
+}
+
+variable "vm_web_core_fraction" {
+  type = number
+  default = 5
+  description = "Гарантированная доля процессора которая будет предоставлена"
+}
+
+variable "vm_web_preemptible" {
+  type = bool
+  default = true
+  description = "в любой момент могут остановить или выключить машину, для тестовых машин подходит"
+}
+
+variable "vm_web_nat" {
+  type = bool
+  default = true
+  description = "нужен для того что бы машина получила адрес  который доступен в интернете а не только внутренний"
+}
+
+variable "vm_web_serial_port" {
+  type = number
+  default = 1
+  description = "Включение серийной косоли"
+}
+
+###vm web vars
+
+variable "vm_db_name" {
+  type = string
+  default = "netology-develop-platform-db"
+  description = "Название виртуальной машины"
+}
+
+variable "vm_db_family" {
+  type = string
+  default = "ubuntu-2004-lts"
+  description = "версия ОС"
+}
+
+variable "vm_db_platform_id" {
+  type = string
+  default = "standard-v1"
+  description = "Тип процессора"
+}
+
+variable "vm_db_cores" {
+  type = number
+  default = 2
+  description = "Количество выделенных ядер процеесора"
+}
+
+variable "vm_db_memory" {
+  type = number
+  default = 2
+  description = "Количество выделенной оперативной памяти"
+}
+
+variable "vm_db_core_fraction" {
+  type = number
+  default = 20
+  description = "Гарантированная доля процессора которая будет предоставлена"
+}
+
+variable "vm_db_preemptible" {
+  type = bool
+  default = true
+  description = "в любой момент могут остановить или выключить машину, для тестовых машин подходит"
+}
+
+variable "vm_db_nat" {
+  type = bool
+  default = true
+  description = "нужен для того что бы машина получила адрес  который доступен в интернете а не только внутренний"
+}
+
+variable "vm_db_serial_port" {
+  type = number
+  default = 1
+  description = "Включение серийной косоли"
+}
+
+variable "vm_db_zone" {
+  type        = string
+  default     = "ru-central1-b"
+  description = "Зона для db"
+}
+
+variable "vpc_name_db" {
+  type = string
+  default = "develop-db"
+  description = "название подсети"
+}
+
+variable "db_cidr" {
+  type = list(string)
+  default = ["10.0.2.0/24"]
+  description = "подсеть отличная от нашей дефолтной"
+}
